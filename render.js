50f56b489469967f993118154e6b9856df6bcbd2
xThatOneAltx <xthxt0neguyx@gmail.com>
2026-02-19 16:22:07 -0600

    Update render.js

--- render.js
+++ render.js
@@ -36,7 +36,7 @@ function flagUrl(countryCode) {
   return `https://flagcdn.com/w40/${String(countryCode).toLowerCase()}.png`;
 }
 
-const TEAMS_LAST_UPDATED = "February 8, 2026";
+const TEAMS_LAST_UPDATED = "February 19, 2026";
 const PLAYERS_LAST_UPDATED = "February 8, 2026";
 
 function setLastUpdated() {
