9420d6349b39566c9d3e786dd9c6156e50ef4ee0
xThatOneAltx <xthxt0neguyx@gmail.com>
2026-02-22 12:43:05 -0600

    Update render.js

--- render.js
+++ render.js
@@ -37,7 +37,7 @@ function flagUrl(countryCode) {
 }
 
 const TEAMS_LAST_UPDATED = "February 22, 2026 - 12:45am EST";
-const PLAYERS_LAST_UPDATED = "February 21, 2026";
+const PLAYERS_LAST_UPDATED = "February 22, 2026 - 12:45am EST";
 
 function setLastUpdated() {
   const t = document.getElementById("teams-updated");
